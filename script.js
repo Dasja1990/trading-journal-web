06fe3f43a14d3afa679d1dd018911535f29decd7
DasjaG <madaig@ukr.net>
2025-11-26 20:53:49 +0100

    feat: add basic trade function

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple function to simulate adding a crypto trade
+function addTrade(asset, amount, price) {
+    console.log(`Trade added: ${asset} | ${amount} units @ $${price}`);
+}
+
+// Example call:
+addTrade("BTC", 0.01, 45000);
